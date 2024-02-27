b0700e0fd7b54e0c5b1285d1d949522da620c60c
Daniel Carlander <36274552+danicrg@users.noreply.github.com>
2024-02-27 22:49:35 +0000

    Training loop

--- bpe.py
+++ bpe.py
@@ -1,6 +1,6 @@
 class BPETokenizer:
-    def __init__(self):
-        pass
+    def __init__(self, vocab_size=276):
+        self.vocab_size = vocab_size
 
     def get_stats(self, ids):
         """
@@ -19,7 +19,6 @@ class BPETokenizer:
         new_ids = []
         i = 0
         while i < len(ids):
-
             if i < len(ids)-1 and ids[i] == pair[0] and ids[i+1] == pair[1]:
                 new_ids.append(idx)
                 i += 2
@@ -29,7 +28,18 @@ class BPETokenizer:
 
         return new_ids
     
+    def train(self, ids):
+        n_iterations = self.vocab_size - 256
+        self.merged = {}
+        new_ids = list(ids)
+        for i in range(n_iterations):
+            stats = self.get_stats(new_ids)
+            top_pair = max(stats, key=stats.get)
+            idx =  256+i
+            new_ids = self.merge(new_ids, top_pair, idx)
+            self.merged[top_pair] = idx
 
+        print("Compression ratio:", len(ids)/len(new_ids))
 
 
 def main():
@@ -40,20 +50,10 @@ def main():
     text = "The Tokenizer is a necessary and pervasive component of Large Language Models (LLMs), where it translates between strings and tokens (text chunks). Tokenizers are a completely separate stage of the LLM pipeline: they have their own training sets, training algorithms (Byte Pair Encoding), and after training implement two fundamental functions: encode() from strings to tokens, and decode() back from tokens to strings. In this lecture we build from scratch the Tokenizer used in the GPT series from OpenAI. In the process, we will see that a lot of weird behaviors and problems of LLMs actually trace back to tokenization. We'll go through a number of these issues, discuss why tokenization is at fault, and why someone out there ideally finds a way to delete this stage entirely."
     ids = text.encode()
     tokenizer = BPETokenizer()
-    print(f"{len(ids)=}")
+    tokenizer.train(ids)
     
-    vocab_size = 275
-    n_iterations = vocab_size - 256
 
-    merged = {}
-    new_ids = list(ids)
-    for _ in range(n_iterations):
-        stats = tokenizer.get_stats(new_ids)
-        top_pair = max(stats, key=stats.get)
-        new_ids = tokenizer.merge(new_ids, top_pair, 256)
-        print(f"{len(new_ids)=}")
 
-    print("Compression:", len(new_ids)/len(ids))
 
 
 
